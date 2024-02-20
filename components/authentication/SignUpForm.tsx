9b494800b637880054b7699c93c8be3a1a009efe
mdasik0 <asikthe1st@gmail.com>
2024-02-20 10:13:35 +0600

    loading component added

--- components/authentication/SignUpForm.tsx
+++ components/authentication/SignUpForm.tsx
@@ -1,71 +1,109 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 import { MdErrorOutline } from "react-icons/md";
 
 const SignUpForm = () => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div className="h-full bg-white rounded-2xl p-9 border-b-8 border-blue-500">
-      <h1 className=" text-4xl">Register</h1>
-      <form className="mt-10">
-        <div className="flex justify-between w-full mb-3">
-          <label className="text-sm flex flex-col" htmlFor="firstName">
-            FirstName
+    <div className="h-full bg-white rounded-2xl p-9 border-b-8 border-blue-500 text-center flex flex-col justify-between">
+      <>
+        <h1 className="text-start text-4xl">Register</h1>
+        <form className="mt-10 flex flex-col items-start">
+          <div className="flex justify-between w-full mb-3">
+            <label
+              className="text-sm flex flex-col items-start"
+              htmlFor="firstName"
+            >
+              FirstName
+              <input
+                className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
+                type="text"
+                name="firstname"
+                placeholder="Enter FirstName"
+              />
+            </label>
+            <label
+              className="text-sm flex flex-col items-start"
+              htmlFor="lastName"
+            >
+              LastName
+              <input
+                className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
+                type="text"
+                name="lastname"
+                placeholder="Enter LastName"
+              />
+            </label>
+          </div>
+          <label
+            className="text-sm flex flex-col items-start mb-3"
+            htmlFor="email"
+          >
+            Email
             <input
               className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
-              type="text"
-              name="firstname"
-              placeholder="Enter FirstName"
+              type="email"
+              name="email"
+              placeholder="Enter Email"
             />
           </label>
-          <label className="text-sm flex flex-col" htmlFor="lastName">
-            LastName
-            <input
-              className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
-              type="text"
-              name="lastname"
-              placeholder="Enter LastName"
-            />
-          </label>
-        </div>
-        <label className="text-sm flex flex-col mb-3" htmlFor="email">
-          Email
-          <input
-            className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
-            type="email"
-            name="email"
-            placeholder="Enter Email"
-          />
-        </label>
-        <div className="flex justify-between w-full mb-5">
-          <label className="text-sm flex flex-col" htmlFor="password">
-            Password
-            <input
-              className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
-              type="password"
-              name="password"
-              placeholder="Enter Password"
-            />
-          </label>
-          <label className="text-sm flex flex-col" htmlFor="password">
-            Confirm Password
-            <input
-              className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
-              type="password"
-              name="password"
-              placeholder="Confirm Password"
-            />
-          </label>
-        </div>
-        <span className=" bg-red-400 text-white pe-3 py-1 rounded text-sm flex items-center w-fit gap-1">
-          <MdErrorOutline className="text-lg ms-2" />
-          Errors show here
-        </span>
-        <button
-          className="bg-blue-500 text-white w-full py-2 rounded mt-3 mb-3"
-          type="submit"
+          <div className="flex justify-between w-full mb-5">
+            <label
+              className="text-sm flex flex-col items-start"
+              htmlFor="password"
+            >
+              Password
+              <input
+                className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+              />
+            </label>
+            <label
+              className="text-sm flex flex-col items-start"
+              htmlFor="password"
+            >
+              Confirm Password
+              <input
+                className="border-2 outline-none focus:outline-blue-500 duration-300 px-3 rounded py-2"
+                type="password"
+                name="password"
+                placeholder="Confirm Password"
+              />
+            </label>
+          </div>
+          <span
+            className={`${
+              error ? "flex" : "hidden"
+            } bg-red-400 text-white pe-3 py-1 rounded text-sm items-center w-fit gap-1`}
+          >
+            <MdErrorOutline className="text-lg ms-2" />
+            {error}
+          </span>
+          <button
+            className="bg-[#1D91CC] hover:bg-[#167db1] duration-300 text-white hover:bgblue w-full py-2 rounded mt-3 mb-3 flex items-center justify-center gap-2"
+            type="submit"
+          >
+            Register
+            {loading && (
+              <span className="loading loading-spinner text-white"></span>
+            )}
+          </button>
+        </form>
+      </>
+      <span className="w-full text-sm mt-auto">
+        Already Registered?{" "}
+        <Link
+          className="hover:underline duration-300 hover:text-blue-500 "
+          href={"/signin"}
         >
-          Register
-        </button>
-        <span className="text-center text-sm">Already Registered? Login!</span>
-      </form>
+          Login!
+        </Link>
+      </span>
     </div>
   );
 };
